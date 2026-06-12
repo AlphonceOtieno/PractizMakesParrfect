a08ccd80d9b6d05b5035b205f43cc085daae4d5d
AlphonceOtieno <alphonceolilo1@gmail.com>
2026-06-12 09:49:48 +0300

    first commit

---
+++ constructors.java
@@ -0,0 +1,12 @@
+public class constructors {
+    int y ;
+    public static  void  main(String[]args){
+       constructors obj = new constructors();
+
+       obj.y = 20;
+       System.out.println(obj.y);
+
+    }
+
+
+}
